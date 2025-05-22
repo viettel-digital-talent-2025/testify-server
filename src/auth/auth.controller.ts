05eb808a30cf98f1c4883754a7aca51175259e65
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-23 06:30:09 +0700

    feat: add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,172 @@
+import {
+  HttpCode,
+  Body,
+  Get,
+  Post,
+  Delete,
+  Controller,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { RegisterDto, LoginDto } from './dtos/auth.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  ForgotPasswordDto,
+  VerifyOtpDto,
+  ResetPasswordDto,
+} from './dtos/recovery-password.dto';
+
+interface RequestWithCookies extends Request {
+  cookies: {
+    refreshToken?: string;
+  };
+}
+
+@ApiTags('api/v1/auth')
+@Controller('api/v1/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(201)
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: RegisterDto })
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid input data' })
+  async register(@Res() res: Response, @Body() body: RegisterDto) {
+    const { accessToken, refreshToken, user } =
+      await this.authService.register(body);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      path: '/',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: 'none',
+      secure: true,
+    });
+
+    return res.json({
+      message: `Welcome aboard, ${user.firstname}! Your account has been created successfully.`,
+      accessToken,
+      user,
+    });
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Login user' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({ status: 200, description: 'User logged in successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  async login(@Res() res: Response, @Body() body: LoginDto) {
+    const { accessToken, refreshToken, user } =
+      await this.authService.login(body);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      path: '/',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: 'none',
+      secure: true,
+    });
+
+    return res.json({
+      message: `Great to see you again, ${user.firstname}!`,
+      accessToken,
+      user,
+    });
+  }
+
+  @Delete('logout')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Logout user' })
+  @ApiResponse({ status: 200, description: 'User logged out successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async logout(@Req() req: RequestWithCookies, @Res() res: Response) {
+    const oldRefreshToken = req.cookies.refreshToken;
+    if (!oldRefreshToken) {
+      throw new UnauthorizedException('No refresh token found');
+    }
+    await this.authService.logout(oldRefreshToken);
+    res.clearCookie('refreshToken');
+
+    return res.json({
+      message: 'User logged out successfully',
+    });
+  }
+
+  @Get('refresh-token')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Refresh access token' })
+  @ApiResponse({ status: 200, description: 'Token refreshed successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid refresh token' })
+  async refreshToken(@Req() req: RequestWithCookies, @Res() res: Response) {
+    const oldRefreshToken = req.cookies.refreshToken;
+    if (!oldRefreshToken) {
+      throw new UnauthorizedException('No refresh token found');
+    }
+
+    const { accessToken, refreshToken } =
+      await this.authService.refreshToken(oldRefreshToken);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      path: '/',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: 'none',
+      secure: true,
+    });
+
+    return res.json({
+      message: 'Refresh token successfully',
+      accessToken,
+    });
+  }
+
+  @Post('forgot-password')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Request password reset' })
+  @ApiBody({ type: ForgotPasswordDto })
+  @ApiResponse({ status: 200, description: 'Password reset email sent' })
+  @ApiResponse({ status: 401, description: 'Account does not exist' })
+  async forgotPassword(@Body() body: ForgotPasswordDto) {
+    await this.authService.forgotPassword(body.email);
+    return {
+      message: 'Password reset instructions have been sent to your email',
+    };
+  }
+
+  @Post('verify-otp')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Verify OTP for password reset' })
+  @ApiBody({ type: VerifyOtpDto })
+  @ApiResponse({ status: 200, description: 'OTP verified successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid or expired OTP' })
+  async verifyOtp(@Body() body: VerifyOtpDto) {
+    await this.authService.verifyOtp(body.email, body.otp);
+    return {
+      message: 'OTP verified successfully',
+    };
+  }
+
+  @Post('reset-password')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Reset password with OTP' })
+  @ApiBody({ type: ResetPasswordDto })
+  @ApiResponse({ status: 200, description: 'Password reset successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid OTP or account' })
+  async resetPassword(@Body() body: ResetPasswordDto) {
+    await this.authService.resetPassword(
+      body.email,
+      body.otp,
+      body.newPassword,
+      body.confirmPassword,
+    );
+    return {
+      message: 'Password has been reset successfully',
+    };
+  }
+}
