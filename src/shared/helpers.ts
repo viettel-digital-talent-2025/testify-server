03dce12a897d1e439238d9f5a8c602834a5a15d8
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-23 06:33:44 +0700

    feat: add prisma helper functions

---
+++ src/shared/helpers.ts
@@ -0,0 +1,19 @@
+import { Prisma } from '@prisma/client';
+
+export function isUniqueConstraintError(
+  error: any,
+): error is Prisma.PrismaClientKnownRequestError {
+  return (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === 'P2002'
+  );
+}
+
+export function isNotFoundError(
+  error: any,
+): error is Prisma.PrismaClientKnownRequestError {
+  return (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === 'P2025'
+  );
+}
