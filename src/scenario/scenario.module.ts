425aaad26efef41cc746c743252f5450ab3b5e0f
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-31 22:58:58 +0700

    feat: add scenario module

--- src/scenario/scenario.module.ts
+++ src/scenario/scenario.module.ts
@@ -1,12 +1,12 @@
+import { LoadTestModule } from '@/load-test/load-test.module';
+import { ScenarioGroupController } from '@/scenario/controllers/scenario-group.controller';
+import { ScenarioController } from '@/scenario/controllers/scenario.controller';
+import { ScenarioGroupRepository } from '@/scenario/repositories/scenario-group.repository';
+import { ScenarioRepository } from '@/scenario/repositories/scenario.repository';
+import { ScenarioGroupService } from '@/scenario/services/scenario-group.service';
+import { ScenarioService } from '@/scenario/services/scenario.service';
+import { PrismaService } from '@/shared/services/prisma.service';
 import { Module } from '@nestjs/common';
-import { LoadTestModule } from '../load-test/load-test.module';
-import { ScenarioController } from './controllers/scenario.controller';
-import { ScenarioGroupController } from './controllers/scenario-group.controller';
-import { ScenarioService } from './services/scenario.service';
-import { ScenarioGroupService } from './services/scenario-group.service';
-import { ScenarioRepository } from './repositories/scenario.repository';
-import { ScenarioGroupRepository } from './repositories/scenario-group.repository';
-import { PrismaService } from '../shared/services/prisma.service';
 
 @Module({
   imports: [LoadTestModule],
