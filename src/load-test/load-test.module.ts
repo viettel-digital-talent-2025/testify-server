3449f0a1a999e25bc07cc5f27f313ef10a3b8948
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-31 22:59:47 +0700

    feat: add load test module

--- src/load-test/load-test.module.ts
+++ src/load-test/load-test.module.ts
@@ -1,10 +1,16 @@
+import { LoadTestsController } from '@/load-test/controllers/load-tests.controller';
+import { MetricsController } from '@/load-test/controllers/metrics.controller';
+import { InfluxDBService } from '@/load-test/services/influxdb.service';
+import { K6Service } from '@/load-test/services/k6.service';
+import { LoadTestsService } from '@/load-test/services/load-tests.service';
+import { MetricsService } from '@/load-test/services/metrics.service';
+import { RunHistoryRepository } from '@/run-history/run-history.repository';
+import { ScenarioRepository } from '@/scenario/repositories/scenario.repository';
+import { PrismaService } from '@/shared/services/prisma.service';
+import { SharedModule } from '@/shared/shared.module';
 import { Module } from '@nestjs/common';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ConfigModule } from '@nestjs/config';
-import { LoadTestService } from './load-test.service';
-import { LoadTestController } from './load-test.controller';
-import { SharedModule } from '../shared/shared.module';
-import { InfluxDBService } from './influxdb.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
@@ -13,7 +19,6 @@ import { InfluxDBService } from './influxdb.service';
       isGlobal: true,
     }),
     EventEmitterModule.forRoot({
-      // Global event emitter options
       wildcard: false,
       delimiter: '.',
       newListener: false,
@@ -23,8 +28,16 @@ import { InfluxDBService } from './influxdb.service';
       ignoreErrors: false,
     }),
   ],
-  controllers: [LoadTestController],
-  providers: [LoadTestService, InfluxDBService],
-  exports: [LoadTestService],
+  controllers: [LoadTestsController, MetricsController],
+  providers: [
+    LoadTestsService,
+    InfluxDBService,
+    MetricsService,
+    PrismaService,
+    K6Service,
+    ScenarioRepository,
+    RunHistoryRepository,
+  ],
+  exports: [LoadTestsService, MetricsService],
 })
 export class LoadTestModule {}
