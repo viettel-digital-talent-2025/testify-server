33ee0146ba6f601184f5fafe92a2917fb7bccfc3
Npeka <npkhang22@clc.fitus.edu.vn>
2025-06-07 01:12:10 +0700

    refactor: update load test types

--- src/load-test/types/load-test.types.ts
+++ src/load-test/types/load-test.types.ts
@@ -1,89 +1,27 @@
-import { ChildProcess } from 'child_process';
+import { RunHistoryStatus } from '@prisma/client';
 
-import {
-  RunHistoryStatus,
-  Scenario,
-  ScenarioFlow,
-  ScenarioFlowStep,
-} from '@prisma/client';
-
-export interface Endpoint {
-  id: string;
-  name: string;
-  method: string;
-  endpoint: string;
-  body?: Record<string, unknown>;
-  headers?: Record<string, string>;
-}
-
-export interface ScenarioWithEndpoints extends Omit<Scenario, 'Flows'> {
-  endpoints: Endpoint[];
-}
-
-export interface K6Metrics {
-  http_req_failed?: {
-    values: {
-      rate: number;
-    };
-  };
-  http_req_duration?: {
-    values: {
-      avg: number;
-    };
-  };
-  http_reqs?: {
-    values: {
-      rate: number;
-    };
-  };
-}
-
-export interface K6Result {
-  metrics: K6Metrics;
-}
-
-export interface ActiveTest {
-  process: ChildProcess;
-  runHistoryId: string;
-}
-
-export interface LoadTestCompletedEvent {
+export interface ActiveJob {
   scenarioId: string;
   runHistoryId: string;
-  status: RunHistoryStatus;
+  runAt: Date;
 }
-
-export interface LoadTestStartedEventInput {
-  scenarioId: string;
-  runHistoryId: string;
-  status?: RunHistoryStatus;
-}
-
-export type ScenarioWithFlowsAndSteps = Scenario & {
-  flows: Array<
-    ScenarioFlow & {
-      steps: Array<
-        ScenarioFlowStep & {
-          config: {
-            method?: string;
-            url?: string;
-            body?: Record<string, unknown>;
-            headers?: Record<string, string>;
-          };
-        }
-      >;
-    }
-  >;
-};
 
 export interface LoadTestStatusEvent {
-  scenarioId: string;
   runHistoryId: string;
+  scenarioId: string;
+  userId: string;
   status: RunHistoryStatus;
-  metrics?: {
-    avgResponseTime: number;
-    errorRate: number;
-    successRate: number;
-    requestsPerSecond: number;
-  };
+  runAt: Date;
+  type: string;
+  id: string;
+  retry: number;
+}
+
+export interface EmitStatusUpdateProps {
+  runHistoryId: string;
+  scenarioId: string;
+  userId: string;
+  status: RunHistoryStatus;
+  runAt: Date;
+  redis?: boolean;
 }
