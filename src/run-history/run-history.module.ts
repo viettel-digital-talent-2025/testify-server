4c0ca392f72a8d4626f411b4ebaf8c993e62afd0
Npeka <npkhang22@clc.fitus.edu.vn>
2025-06-07 01:15:28 +0700

    refactor: update run history module

--- src/run-history/run-history.module.ts
+++ src/run-history/run-history.module.ts
@@ -1,14 +1,20 @@
+import { RunHistoryRepository } from '@/run-history/repositories/run-history.repository';
 import { RunHistoryController } from '@/run-history/run-history.controller';
-import { RunHistoryRepository } from '@/run-history/run-history.repository';
 import { RunHistoryService } from '@/run-history/run-history.service';
 import { PrismaService } from '@/shared/services/prisma.service';
 import { SharedModule } from '@/shared/shared.module';
 import { Module } from '@nestjs/common';
+import { RunHistoryMetricsRepository } from './repositories/run-history-metrics.repository';
 
 @Module({
   imports: [SharedModule],
   controllers: [RunHistoryController],
-  providers: [RunHistoryService, RunHistoryRepository, PrismaService],
+  providers: [
+    PrismaService,
+    RunHistoryService,
+    RunHistoryRepository,
+    RunHistoryMetricsRepository,
+  ],
   exports: [RunHistoryService],
 })
 export class RunHistoryModule {}
